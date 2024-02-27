6e1143db6df7c1ec49df1d332e138809e8a9f84f
Mhjba <mahjoubaaitalla15@gmail.com>
2024-02-27 08:56:31 -0800

    new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,24 +1,24 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Create a binary node
- * @parent: node parent
- * @value: Value of the node
- * Return: the new node
+ * binary_tree_node - Creates a binary tree node.
+ * @parent: pointer to the parent node.
+ * @value: value to the node.
+ *
+ * Return: a pointer to the new node, or NULL on failure.
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *node;
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
-	{
+    node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
 		return (NULL);
-	}
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	return (new_node);
+
+    node->parent = parent;
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+
+	return (node);
 }
