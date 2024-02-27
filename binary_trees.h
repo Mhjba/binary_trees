11fb530761f87a71b2a225aca8d3d1fd84a41811
Mhjba <mahjoubaaitalla15@gmail.com>
2024-02-27 08:51:14 -0800

    new node

--- binary_trees.h
+++ binary_trees.h
@@ -26,21 +26,8 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
-/*  linked list for advanced 101 the levelorder */
-/**
- * struct link_s - structure for advanced tasks
- *
- * @n: depth of node specified
- * @node: node of tree to store
- * @next: next node of the linked list
- */
-typedef struct link_s
-{
-	size_t n;
-	struct binary_tree_s const *node;
-	struct link_s *next;
-} link_t;
-/*  ----------------- mandatory task ---------------------------------*/
+
+
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -84,4 +71,4 @@ heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
-#endif /* _BINARY_TREES_H_ */
+#endif 
