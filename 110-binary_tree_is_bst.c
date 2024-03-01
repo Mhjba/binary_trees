75bcb7731a11fea27cae5c5a9ce61a9f0f9097af
Mhjba <mahjoubaaitalla15@gmail.com>
2024-03-01 09:14:31 -0800

    binary tree

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,48 +1,48 @@
 #include "binary_trees.h"
 
 /**
- * find_node -function find node in a tree
- * @root: root of the tre to evaluate
+ * find_node - function to find a node in a tree
+ * @root: root of the tree to evaluate
  * @node: node to find
- * Return: 1 if exits 0 if no
+ * Return: 1 if exists, 0 if not
  */
 int find_node(binary_tree_t *root, binary_tree_t *node)
 {
-	if (root == NULL)
+	if (root == NULL || node == NULL)
 		return (0);
 	if (node == root)
 		return (1);
 	return (node->n < root->n ? find_node(root->left, node) :
 								find_node(root->right, node));
-	return (0);
 }
 
 /**
- * croos_tree - cross the tree checking if each node exist correctly
+ * cross_tree - recursively check if each node exists correctly
  * @root: root node of the tree
  * @node: current node to evaluate
- * Return: 1 if is BST0 if no
+ * Return: 1 if it is a BST, 0 if not
  */
-int croos_tree(binary_tree_t *root, binary_tree_t *node)
+int cross_tree(binary_tree_t *root, binary_tree_t *node)
 {
 	int aux = 0;
+
+	if (root && node)
 	{
-		if (root && node)
 		aux = find_node(root, node);
 		if (node->left && node->right)
-			aux &= croos_tree(root, node->left) && croos_tree(root, node->right);
-		return (aux);
+			aux &= cross_tree(root, node->left) && cross_tree(root, node->right);
 	}
+	return (aux);
 }
 
 /**
- * binary_tree_is_bst - check if ist a correctly bst tree
- * @tree: tre to check
- * Return: 1 if is bst 0 if not
+ * binary_tree_is_bst - check if it is a correctly BST tree
+ * @tree: tree to check
+ * Return: 1 if it is a BST, 0 if not
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	return (croos_tree((binary_tree_t *)tree, (binary_tree_t *)tree));
+	return (cross_tree((binary_tree_t *)tree, (binary_tree_t *)tree));
 }
